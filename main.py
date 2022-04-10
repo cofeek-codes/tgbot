97a66f2a7eb1de0efe2a45563b782c345ab4b809
cofeek-codes <kormyshev11@mail.ru>
2022-04-10 17:22:51 +0500

    new

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from email import message
 import telebot
 token = '5233084458:AAE79zJ8kI3Fx7nNmGkpTHI6pyRE-FcyGkI'
 bot = telebot.TeleBot(token)
@@ -9,8 +10,10 @@ bot = telebot.TeleBot(token)
 def messanger(message):
     if message.text == 'hello':
         bot.send_message(message.from_user.id, 'hello, friend')
+    elif message.text == '/help':
+        bot.send_message(message.from_user.id, 'type \'hello\'')
     else:
-        bot.send_message(message.from_user.id, 'do i know you?')
+        bot.send_message(message.from_user.id, 'type \'/help\'')
 
 
 bot.polling(non_stop=True, interval=0)
