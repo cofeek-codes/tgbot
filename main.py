a2d82bd2e5895768a4e24406d08bcf29e9bf11b7
cofeek-codes <kormyshev11@mail.ru>
2022-05-24 20:32:14 +0500

    parser bot start

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 from email import message
 import telebot
-token = '5233084458:AAE79zJ8kI3Fx7nNmGkpTHI6pyRE-FcyGkI'
-bot = telebot.TeleBot(token)
 
 
-@bot.message_handler(commands=['start', 'help'])
-def start(message):
-    mes = f'hello, {message.from_user.first_name}'
-    bot.send_message(message.chat.id, mes)
+def main():
+
+    token = '5233084458:AAE79zJ8kI3Fx7nNmGkpTHI6pyRE-FcyGkI'
+    bot = telebot.TeleBot(token)
+
+    bot.polling(non_stop=True, interval=0)
 
 
-bot.polling(non_stop=True, interval=0)
+if __name__ == "__main__":
+    main()
