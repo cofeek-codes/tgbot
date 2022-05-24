c08be48f120df80f768c2364cb2f22669ec4b43a
cofeek-codes <kormyshev11@mail.ru>
2022-05-24 20:51:24 +0500

    parser bot start

--- main.py
+++ main.py
@@ -1,12 +1,18 @@
 from email import message
 import telebot
 
+from parser import parse
+
+
+token = '5233084458:AAE79zJ8kI3Fx7nNmGkpTHI6pyRE-FcyGkI'
+bot = telebot.TeleBot(token)
+
+url = ''
+
 
 def main():
-
-    token = '5233084458:AAE79zJ8kI3Fx7nNmGkpTHI6pyRE-FcyGkI'
-    bot = telebot.TeleBot(token)
-
+    global token, bot, url
+    parse(url)
     bot.polling(non_stop=True, interval=0)
 
 
