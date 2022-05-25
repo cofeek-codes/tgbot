383a4547cf578aaebcf92dea04026acb3b3a0179
cofeek-codes <kormyshev11@mail.ru>
2022-05-25 22:54:34 +0500

    bot

---
+++ bot/bot.py
@@ -0,0 +1,28 @@
+import telebot
+
+from parser.parser import parse
+
+token = '5233084458:AAE79zJ8kI3Fx7nNmGkpTHI6pyRE-FcyGkI'
+
+bot = telebot.TeleBot(token)
+
+# projects
+url = 'https://profile-psi-three.vercel.app/'
+
+
+@bot.message_handler(commands=['projects'])
+def get_projects(message):
+    global url
+
+
+# greet
+
+
+@bot.message_handler(commands=['hello'])
+def greet(message):
+    bot.send_message(message.chat.id, 'hello, {}'.format(
+        message.from_user.first_name))
+
+
+print('bot is working...')
+bot.infinity_polling()
--- main.py
+++ main.py
@@ -1,16 +1,15 @@
+from bot.bot import get_updates
 from parser.parser import parse
 
-
-token = '5233084458:AAE79zJ8kI3Fx7nNmGkpTHI6pyRE-FcyGkI'
 
 url = 'https://profile-psi-three.vercel.app/'
 
 
 def main():
-    global token, url
+    global url
     titles, skills = parse(url)
-    for i, title in enumerate(titles):
-        print(title, skills[i])
+
+    get_updates()
 
 
 if __name__ == "__main__":
