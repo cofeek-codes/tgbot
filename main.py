ea12a9b906040301a6b2c93cc58ed3517dc9b813
cofeek-codes <kormyshev11@mail.ru>
2022-05-25 19:24:46 +0500

    parser

--- main.py
+++ main.py
@@ -1,19 +1,14 @@
-from email import message
-import telebot
-
 from parser import parse
 
 
 token = '5233084458:AAE79zJ8kI3Fx7nNmGkpTHI6pyRE-FcyGkI'
-bot = telebot.TeleBot(token)
 
-url = ''
+url = 'https://profile-psi-three.vercel.app/'
 
 
 def main():
-    global token, bot, url
+    global token, url
     parse(url)
-    bot.polling(non_stop=True, interval=0)
 
 
 if __name__ == "__main__":
--- parser.py
+++ parser.py
@@ -1,2 +1,16 @@
+import imp
+import requests
+import lxml.html
+from lxml.html import etree
+
+
 def parse(url):
-    pass
+    res = requests.get(url)
+    if res.status_code == 200:
+
+        tree = lxml.html.document_fromstring(res.text)
+        titles = tree.xpath("//*[@class='project__name']/text()")
+        skills = tree.xpath("//*[@class='circle__title']/text()")
+        return titles, skills
+    else:
+        print(f'connection error: {res.status_code}')
